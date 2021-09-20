f593d30d56b97984618d0a18d9d2d5aa45e3d7f0
Cindy H <cindyhaa@gmail.com>
2021-09-20 23:00:01 +0000

    changing exports to objects

---
+++ index.js
@@ -0,0 +1,16 @@
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArray');
+const head = require('./head');
+const middle = require('./middle');
+const tail = require('./tail');
+
+
+module.exports = {
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  eqArray: eqArrays,
+  head: head,
+  middle: middle,
+  tail: tail
+};
