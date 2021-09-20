228c00e9be13fad19a8f1e782e5d1135b460e6f7
Cindy H <cindyhaa@gmail.com>
2021-09-20 19:50:55 +0000

    add testing file, module exporting & requiring

--- tail.js
+++ tail.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`💯💯💯 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual')
 
 const tail = function(words) {
   for (let i = 0; i <= words.length; i++) {
@@ -12,6 +6,4 @@ const tail = function(words) {
   }
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs", "Violet"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 4); // original array should still have 3 elements!
+module.exports = tail;
